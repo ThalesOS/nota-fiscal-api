e165c3075304db5ac1af711e9136bca9125415da
Thales Oliveira <thales.oliveira2567@gmail.com>
2026-07-23 00:00:46 -0300

    Implementação do dto de erros

---
+++ src/main/java/com/notafiscalapi/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.notafiscalapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private String error;
+    private HttpStatus status;
+    private String path;
+}
